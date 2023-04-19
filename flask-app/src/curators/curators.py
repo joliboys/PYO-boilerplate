e2a604da7a0c21c7a889032a5e30cd4cbc371e25
nihezagirwe <nihezagirwe.k@northeastern.edu>
2023-04-19 01:06:28 -0400

    get eng

--- flask-app/src/curators/curators.py
+++ flask-app/src/curators/curators.py
@@ -19,3 +19,29 @@ def get_curators():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+@curators.route('/posteng', methods=['GET'])
+def get_eng():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for the list of interactions
+    cursor.execute('SELECT Views, Interactions FROM PostEngagement')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
