fe1e33c3863431dfa5702a516fc089c2b367aec1
joliboys <aidensoccer03@gmail.com>
2023-04-19 01:29:42 -0400

    edit string

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -27,7 +27,7 @@ def create_app():
     # Add a default route
     @app.route("/")
     def welcome():
-        return "<h1>Welcome to the 3200 boilerplate app</h1>"
+        return "<h1>Welcome to the PYO boilerplate app</h1>"
 
     # Import the various routes
     from src.views import views
