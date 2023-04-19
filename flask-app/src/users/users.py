2c27b7684d5b63f742433e2cb0e4b93251d8314a
joliboys <aidensoccer03@gmail.com>
2023-04-18 23:48:00 -0400

    setup for src

---
+++ flask-app/src/users/users.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+
+users = Blueprint('users', __name__)
+
+# Get all customers from the DB
+@artists.route('/users', methods=['GET'])
+def get_customers():
+    cursor = db.get_db().cursor()
+    cursor.execute()
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
