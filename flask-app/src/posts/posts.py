29c429ae5b8d6a2dcf6a48ab0773837513aaacb6
suryaramak2 <srk03.srk03@gmail.com>
2023-04-20 11:17:44 -0400

    add song and add post

--- flask-app/src/posts/posts.py
+++ flask-app/src/posts/posts.py
@@ -9,7 +9,7 @@ posts = Blueprint('Posts', __name__)
 @posts.route('/deletepost/<int:post_id>', methods=['DELETE'])
 def delete_post(post_id):
     cursor = db.get_db().cursor()
-    cursor.execute('DELETE FROM posts WHERE id = %s', (post_id,))
+    cursor.execute('DELETE FROM Posts WHERE id = %s', (post_id,))
     db.get_db().commit()
     return 'Post {} has been deleted.'.format(post_id)
 
@@ -23,7 +23,7 @@ def create_post():
 
     # Insert the new post into the database
     cursor = db.get_db().cursor()
-    cursor.execute('INSERT INTO posts (Genre_ID, Prompt_ID, Song_ID1, Song_ID2, Song_ID3, Song_ID4, User_ID) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+    cursor.execute('INSERT INTO Posts (Genre_ID, Prompt_ID, Song_ID1, Song_ID2, Song_ID3, Song_ID4, User_ID) VALUES (%s, %s, %s, %s, %s, %s, %s)',
                (data['Genre_ID'], data['Prompt_ID'], data['Song_ID1'], data['Song_ID2'], data['Song_ID3'], data['Song_ID4'], data['User_ID']))
     db.get_db().commit()
 
@@ -40,7 +40,7 @@ def update_post(post_id):
 
     # Update the post in the database
     cursor = db.get_db().cursor()
-    cursor.execute('UPDATE posts SET Genre_ID = %s, Prompt_ID = %s, Song_ID1 = %s, Song_ID2 = %s, Song_ID3 = %s, Song_ID4 = %s, User_ID = %s WHERE Post_ID = %s',
+    cursor.execute('UPDATE Posts SET Genre_ID = %s, Prompt_ID = %s, Song_ID1 = %s, Song_ID2 = %s, Song_ID3 = %s, Song_ID4 = %s, User_ID = %s WHERE Post_ID = %s',
                (data['Genre_ID'], data['Prompt_ID'], data['Song_ID1'], data['Song_ID2'], data['Song_ID3'], data['Song_ID4'], data['User_ID'], post_id))
     db.get_db().commit()
 
---
+++ flask-app/src/songs/songs.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+songs = Blueprint('Songs', __name__)
+
+# add a new song
+
+@songs.route('/addsong', methods=['POST'])
+def create_post():
+    # Get the data from the request
+    data = request.get_json()
+
+    # Insert the new song into the database
+    cursor = db.get_db().cursor()
+    cursor.execute('INSERT INTO Songs (Genre_ID, Artist_ID, Name) VALUES (%s, %s, %s)',
+               (data['Genre_ID'], data['Artist_ID'], data['Name']))
+    db.get_db().commit()
+
+    # Return a response indicating that the post has been created
+    return jsonify({'message': 'Post created successfully.'})
