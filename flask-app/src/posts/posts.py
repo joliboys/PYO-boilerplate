93acc2745e7cb09c8b657fce1d317759f80f3545
joliboys <aidensoccer03@gmail.com>
2023-04-20 14:21:23 -0400

    get genre

--- flask-app/src/posts/posts.py
+++ flask-app/src/posts/posts.py
@@ -125,3 +125,19 @@ def get_liked_posts(user_id):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+# Get all genres
+@posts.route('/genre', methods=['GET'])
+def get_genre():
+cursor = db.get_db().cursor()
+cursor.execute("SELECT DISTINCT Name FROM Genre")
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
